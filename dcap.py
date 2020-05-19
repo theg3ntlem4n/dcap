f83a628c2cbc85671043cee27737dc1e4bf01a53
theg3ntlem4n <ar3x.k1m@gmail.com>
2020-05-19 14:36:29 -0400

    moved extractor to new file, improved accuracy

--- dcap.py
+++ dcap.py
@@ -3,6 +3,7 @@
 import cv2
 import time
 from display import Display
+from extractor import FeatureExtractor
 import numpy as np
 import sys
 
@@ -12,26 +13,6 @@ H = 1080*2//5
 diff = []
 
 disp = Display(W, H*2)
-
-class FeatureExtractor(object):
-    def __init__(self):
-        self.orb = cv2.ORB_create(100)
-        self.bf = cv2.BFMatcher()
-        self.last = None
-
-    def extract(self, img):
-        feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, 
-            qualityLevel = 0.01, minDistance= 3)
-        kps = [cv2.KeyPoint(x=f[0][0], y = f[0][1], _size=20) for f in feats]
-        kps, des = self.orb.compute(img, kps)
-        
-        if self.last is not None:
-            matches = self.bf.match(des, self.last['des'])
-            #print(matches)        
-
-        self.last = {'kps': kps, 'des':des}
-
-        return feats, kps, des
 
 fe = FeatureExtractor()
 
@@ -46,14 +27,15 @@ def percent_error(feats_original, feats_test):
     temp_y = abs(test_y - original_y)/original_y
 
     return (temp_x + temp_y)*100
+
 def process_frame(img_original, img_test, accuracy_range):
 
     img_original = cv2.resize(img_original, (W, H))
     img_test = cv2.resize(img_test, (W, H))
 
 
-    feats_original, kps_original, des_original = fe.extract(img_original)
-    feats_test, kps_test, des_test = fe.extract(img_test)
+    feats_original, kps_original, des_original, matches_original = fe.extract(img_original)
+    feats_test, kps_test, des_test, matches_test = fe.extract(img_test)
 
     for p in kps_original:
         u, v = map(lambda x: int(round(x)), p.pt)
@@ -62,6 +44,8 @@ def process_frame(img_original, img_test, accuracy_range):
     for p2 in kps_test:
         a, b = map(lambda y: int(round(y)), p2.pt)
         cv2.circle(img_test, (a, b), color = (0,255,0), radius = 3)
+
+    print(str(len(feats_test))+ ", " + str(len(feats_original))) 
     
     #check for accuracy
 
---
+++ extractor.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+class FeatureExtractor(object):
+    def __init__(self):
+        self.orb = cv2.ORB_create(100)
+        self.bf = cv2.BFMatcher()
+        self.last = None
+
+    def extract(self, img):
+        #detect
+        feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), maxCorners = 500, 
+            qualityLevel = 0.001, minDistance= 3)
+
+        #extraction
+
+        kps = [cv2.KeyPoint(x=f[0][0], y = f[0][1], _size=20) for f in feats]
+        kps, des = self.orb.compute(img, kps)
+        
+        #matching
+        matches = None
+        if self.last is not None:
+            matches = self.bf.match(des, self.last['des'])
+            #print(matches)        
+
+        self.last = {'kps': kps, 'des':des}
+
+        return feats, kps, des, matches
+
